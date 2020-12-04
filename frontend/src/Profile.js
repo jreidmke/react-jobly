47eb77f266a7796d970faea601b9be01f28df33f
unknown <jreidmke@gmail.com>
2020-12-04 13:59:42 -0600

    init profile.js

---
+++ frontend/src/Profile.js
@@ -0,0 +1,11 @@
+import JoblyApi from './JoblyApi';
+
+const Profile = () => {
+    return(
+        <div>
+            <h1>This is a profile.</h1>
+        </div>
+    )
+}
+
+export default Profile;
