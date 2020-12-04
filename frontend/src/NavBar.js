5e2c83c0742a437f2f2b7d6cd970043e03f194be
unknown <jreidmke@gmail.com>
2020-12-04 13:46:45 -0600

    init navbar component

---
+++ frontend/src/NavBar.js
@@ -0,0 +1,12 @@
+import {NavLink} from "react-router-dom";
+
+function NavBar() {
+    return(
+        <nav>
+            <NavLink exact to="/">Jobly</NavLink>
+            <NavLink exact to="/companies">Companies</NavLink>
+            <NavLink exact to="/jobs">Jobs</NavLink>
+            <NavLink exact to="/profile">Profile</NavLink>
+        </nav>
+    )
+}
