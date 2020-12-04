828d32bbfeb47a900ad16c11efc5d63da5e76d71
unknown <jreidmke@gmail.com>
2020-12-04 13:59:20 -0600

    init joblist.js

---
+++ frontend/src/JobList.js
@@ -0,0 +1,11 @@
+import JoblyApi from './JoblyApi';
+
+const JobList = () => {
+    return(
+        <div>
+            <h1>This is a Job list.</h1>
+        </div>
+    )
+}
+
+export default JobList;
