060b9c95b0cce275c4860f7bbdd66456581095ed
unknown <jreidmke@gmail.com>
2020-12-04 13:59:32 -0600

    init LoginForm.js

---
+++ frontend/src/LoginForm.js
@@ -0,0 +1,11 @@
+import JoblyApi from './JoblyApi';
+
+const LoginForm = () => {
+    return(
+        <div>
+            <h1>This is a Login Form.</h1>
+        </div>
+    )
+}
+
+export default LoginForm;
