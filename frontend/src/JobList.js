4a2933aad6c42304fc69625e878df383fae7bae7
unknown <jreidmke@gmail.com>
2020-12-04 15:56:46 -0600

    imported allJobs api req into JobList component

--- frontend/src/JobList.js
+++ frontend/src/JobList.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from 'react';
 import JoblyApi from './JoblyApi';
+import {useEffect, useState} from 'react';
 
 const JobList = () => {
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        async function getJobs() {
+            const jobResp = await JoblyApi.allJobs();
+            console.log(jobResp);
+            setJobs(jobResp);
+        }
+        getJobs();
+    }, []);
+
     return(
         <div>
             <h1>This is a Job list.</h1>
