7f5102f50824e712c8ebed913d9b3234d9904957
unknown <jreidmke@gmail.com>
2020-12-04 15:58:59 -0600

    about to create job cards

--- frontend/src/JobList.js
+++ frontend/src/JobList.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import JoblyApi from './JoblyApi';
-import {useEffect, useState} from 'react';
 
 const JobList = () => {
     const [jobs, setJobs] = useState([]);
