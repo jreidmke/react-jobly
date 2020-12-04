226d985d87e5de2ee7fc06b0d518a76d2069c92d
unknown <jreidmke@gmail.com>
2020-12-04 14:53:28 -0600

    implemented getCompany api call

--- frontend/src/Company.js
+++ frontend/src/Company.js
@@ -1,6 +1,18 @@
 import JoblyApi from './JoblyApi';
+import {useEffect, useState, useParams} from 'react';
 
 const Company = () => {
+    const {handle} = useParams();
+    const [company, setCompany] = useState(null);
+
+    useEffect(() => {
+        async function getCompany() {
+            const company = await JoblyApi.getCompany(handle);
+            console.log(company);
+            setCompany(company);
+        }
+    }, []);
+
     return(
         <div>
             <h1>This is a company.</h1>
