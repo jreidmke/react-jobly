dd797e98bb6dfe96557deba58bc5f4c592547c95
unknown <jreidmke@gmail.com>
2020-12-09 06:53:13 -0600

    Imported USerContext component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import JoblyApi from "./JoblyApi";
 import { decode } from "jsonwebtoken";
+import UserContext from "./UserContext";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -20,6 +21,7 @@ function App() {
         let user = await JoblyApi.getCurrentUser(username);
         setUser(user);
       } catch (error) {
+        console.log(error);
         setUser(null);
       }
     }
@@ -30,8 +32,10 @@ function App() {
     <div className="App">
       {console.log(user)}
       <BrowserRouter>
+      <UserContext.Provider value={{ user, setUser }}>
         <NavBar/>
         <Routes/>
+      </UserContext.Provider>
       </BrowserRouter>
     </div>
   );
