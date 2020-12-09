1e99e8b54756bf82d4299767e653b74151a77b7c
unknown <jreidmke@gmail.com>
2020-12-09 07:06:12 -0600

    updated ProtectedRoute comp with correct var names

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -2,16 +2,16 @@ import { useContext } from "react";
 import UserContext from "./UserContext";
 import { Route, Redirect } from "react-router-dom";
 
-const ProtectedRoute = ({ exact, path, component }) => {
+const ProtectedRoute = ({ exact, path, children }) => {
     const { user } = useContext(UserContext);
 
     if(!user) {
-        return<Redirect to="/login"/>
+        return <Redirect to="/login"/>
     };
 
     return (
         <Route exact={exact} path={path}>
-          {component}
+          {children}
         </Route>
       );
 }
