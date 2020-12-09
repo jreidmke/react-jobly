df66ef6b6391c2251c824bb7bc020c0d6e59b5d6
unknown <jreidmke@gmail.com>
2020-12-09 08:23:31 -0600

    init applied state

--- frontend/src/JobList.js
+++ frontend/src/JobList.js
@@ -2,9 +2,14 @@ import { useEffect, useState } from 'react';
 import JoblyApi from './JoblyApi';
 import JobCard from './JobCard';
 import { v4 as uuid_v4 } from "uuid";
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
 const JobList = () => {
+    const { user } = useContext(UserContext);
+
     const [jobs, setJobs] = useState([]);
+    const [applied, setApplied] = useState(user.applications);
 
     useEffect(() => {
         async function getJobs() {
@@ -17,19 +22,21 @@ const JobList = () => {
 
     async function apply(jobIdx) {
         let jobId = jobs[jobIdx].id;
-        
+        let resp = await JoblyApi.applyToJob(user.username, jobId);
+        console.log(resp);
+        // setJobs(j => j.map(job => job.id===jobId ? {...job, isActive: resp} : job));
     }
 
     return(
         <div>
-            {jobs ? jobs.map(j =>
+            {jobs ? jobs.map((j, idx) =>
             <JobCard
             title={j.title}
             salary={j.salary}
             equity={j.equity}
             company_handle={j.company_handle}
             key={uuid_v4()}
-            handleApply={apply}
+            handleApply={() => apply(idx)}
             />
             ) : 'Loading...'}
         </div>
