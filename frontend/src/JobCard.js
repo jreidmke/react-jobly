e74dd8f451bb1e4efb9bee92596755e71b1dd4e8
unknown <jreidmke@gmail.com>
2020-12-09 08:00:35 -0600

    added handleApply method and button to rendering

--- frontend/src/JobCard.js
+++ frontend/src/JobCard.js
@@ -1,11 +1,11 @@
-const JobCard = ({title, salary, equity, company_handle}) => {
+const JobCard = ({title, salary, equity, company_handle, handleApply, isActive}) => {
     return(
-        <div className='card
-        '>
+        <div className='card'>
             <h1>{title}</h1>
             <h3>Salary: {salary}</h3>
             <h3>Equity: {equity}</h3>
             <h4>{company_handle} </h4>
+            <button onClick={handleApply} disabled={isActive}>{isActive ? "Applied" : "Apply"}</button>
         </div>
     )
 }
